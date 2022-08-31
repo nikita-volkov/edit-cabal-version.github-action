f236762a930a1390128429cf1054b603b16d7040
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-31 22:36:24 +0300

    Sketch out the skeleton

--- builds/app/edit-cabal-version/EditCabalVersion.hs
+++ builds/app/edit-cabal-version/EditCabalVersion.hs
@@ -1,3 +1,39 @@
 module EditCabalVersion where
 
-import Coalmine.Prelude
+import Coalmine.Prelude hiding (parseVersion)
+
+editFile :: Path -> (Text -> Either Text Text) -> IO ()
+editFile =
+  error "TODO"
+
+parseVersion :: Text -> Either Text [Int]
+parseVersion =
+  error "TODO"
+
+setVersion :: [Int] -> Text -> Text
+setVersion =
+  error "TODO"
+
+renderVersion :: [Int] -> Text
+renderVersion =
+  error "TODO"
+
+bumpVersionInText ::
+  -- | Index of the bumped version section.
+  -- Major, Minor, Patch and etc.
+  Int ->
+  Text ->
+  Either Text Text
+bumpVersionInText position text =
+  parseVersion text
+    <&> bumpVersion position
+    <&> renderVersion
+
+bumpVersion ::
+  -- | Index of the bumped version section.
+  -- Major, Minor, Patch and etc.
+  Int ->
+  [Int] ->
+  [Int]
+bumpVersion =
+  error "TODO"
