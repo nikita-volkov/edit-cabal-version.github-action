9650972408693540779875a4b5027cfe2a94f1e4
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-09-03 16:17:42 +0300

    Add test

--- app/test/Main.hs
+++ app/test/Main.hs
@@ -1,9 +1,38 @@
 module Main where
 
+import qualified Coalmine.NumericVersion as NumericVersion
 import Coalmine.Prelude
 import Coalmine.Tasty
+import qualified EditCabalVersion as Editor
 
 main =
-  defaultMain . testGroup "All"
-    =<< sequence
-      []
+  defaultMain . testGroup "Editor" $
+    [ eqTestCase
+        "set"
+        ( Right
+            [i|
+              cabal-version: 3.0
+
+              name: test
+              version: 2.0
+
+              common common-settings
+                default-language: Haskell2010
+            |]
+        )
+        ( fmap
+            Editor.modifiedResultText
+            ( Editor.setVersion
+                [NumericVersion.lit|2.0|]
+                [i|
+                cabal-version: 3.0
+
+                name: test
+                version: 0
+
+                common common-settings
+                  default-language: Haskell2010
+              |]
+            )
+        )
+    ]
