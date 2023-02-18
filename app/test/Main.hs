79a430b5c3d3d7dc5122eaf430d6c6f530049488
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-02-19 00:01:39 +0300

    Include bump in the test suite

--- app/test/Main.hs
+++ app/test/Main.hs
@@ -5,12 +5,15 @@ import Coalmine.Prelude
 import Coalmine.Tasty
 import qualified EditCabalVersion as Editor
 
+main :: IO ()
 main =
   defaultMain . testGroup "Editor" $
-    [ eqTestCase
-        "set"
-        ( Right
-            [i|
+    [ testGroup
+        "Set Mode"
+        [ eqTestCase
+            "1"
+            ( Right
+                [i|
               cabal-version: 3.0
 
               name: test
@@ -19,12 +22,12 @@ main =
               common common-settings
                 default-language: Haskell2010
             |]
-        )
-        ( fmap
-            Editor.modifiedResultText
-            ( Editor.setVersion
-                [NumericVersion.lit|0.2|]
-                [i|
+            )
+            ( fmap
+                Editor.modifiedResultText
+                ( Editor.setVersion
+                    [NumericVersion.lit|0.2|]
+                    [i|
                   cabal-version: 3.0
 
                   name: test
@@ -33,6 +36,18 @@ main =
                   common common-settings
                     default-language: Haskell2010
                 |]
+                )
             )
-        )
+        ],
+      testGroup
+        "Bump Mode"
+        [ eqTestCase
+            "New place"
+            (Right "version: 0.2.1")
+            (fmap Editor.modifiedResultText (Editor.bumpVersion 2 "version: 0.2")),
+          eqTestCase
+            "New place deep"
+            (Right "version: 0.0.1")
+            (fmap Editor.modifiedResultText (Editor.bumpVersion 2 "version: 0"))
+        ]
     ]
